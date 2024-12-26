1a30530c9a1188bd15ed0ea9cd43f86a42245600
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-26 09:13:26 +0800

    Update index.tsx

--- src/components/Loading/BrandTextLoading/LobeChatText/index.tsx
+++ src/components/Loading/BrandTextLoading/LobeChatText/index.tsx
@@ -1,6 +1,6 @@
 import LobeChatText from './SVG';
 import './style.css';
 
-const LobeChatTextLoading = () => <LobeChatText size={40} />;
+const LobeChatTextLoading = () => <LobeChatText size={0} />;
 
 export default LobeChatTextLoading;
